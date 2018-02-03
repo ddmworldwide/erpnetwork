a2ef6be9cc8e50bb52a78bf6f43eff6338c3836d
Marius <marius.ndini@sap.com>
2018-02-03 11:08:30 -0500

    init git
    initial git push
    General biz network

---
+++ erpnetwork/README.md
@@ -0,0 +1,1 @@
+# org.erp.mari
---
+++ erpnetwork/lib/logic.js
@@ -0,0 +1,19 @@
+'use strict';
+/**
+ * Sample transaction
+ * @param {org.erp.mari.ChangeAssetValue} changeAssetValue
+ * @transaction
+ */
+function onChangeAssetValue(changeAssetValue) {
+    var assetRegistry;
+    var id = changeAssetValue.relatedAsset.assetId;
+    return getAssetRegistry('org.erp.mari.SampleAsset')
+        .then(function(ar) {
+            assetRegistry = ar;
+            return assetRegistry.get(id);
+        })
+        .then(function(asset) {
+            asset.value = changeAssetValue.newValue;
+            return assetRegistry.update(asset);
+        });
+}
